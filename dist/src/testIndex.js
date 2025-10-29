95b828a2b2cce4aba5cba36e5776bf365cf4c384
alexanh-webd <alexanhfvn@gmail.com>
2025-10-30 01:03:44 +0200

    ex3done

--- dist/src/testIndex.js
+++ dist/src/testIndex.js
@@ -1,21 +1,35 @@
 import { Router } from "express";
+import express from "express";
 //import fs from "fs"
 const router = Router();
 let poems = [];
+router.use(express.json());
 router.get("/hello", (req, res) => {
     res.json({ msg: "Hello World!" });
 });
 router.get("/", (req, res) => {
     res.json({ msg: "Server is running!" });
 });
-router.get("/:id", (req, res) => {
-    let id = parseInt(req.params.id ?? "0");
+router.get("/echo/:id", (req, res) => {
+    let value = req.params.id;
     try {
-        res.json(poems[id]);
+        res.json({ id: value });
     }
     catch (error) {
         console.error(`Error parsing JSON: ${error}`);
     }
+});
+router.post("/sum", (req, res) => {
+    const numbersArr = req.body.numbers;
+    console.log(numbersArr);
+    let result = 0;
+    //for (let i = 0; i < numbersArr.length; i++) {
+    //  result += numbersArr[i]
+    //}
+    for (const num of numbersArr) {
+        result += num;
+    }
+    res.json({ sum: result });
 });
 export default router;
 //# sourceMappingURL=testIndex.js.map
--- src/testIndex.ts
+++ src/testIndex.ts
@@ -1,27 +1,42 @@
 import type {Request, Response} from "express";
 import {Router} from "express";
+import express from "express"
 //import fs from "fs"
 
 const router: Router = Router()
 let poems: string[] = []
 
+router.use(express.json());
 
 router.get("/hello", (req: Request, res: Response) => {
     res.json({msg: "Hello World!"})
 })
+
 router.get("/", (req: Request, res: Response) => {
   res.json({ msg: "Server is running!" });
 });
 
-router.get("/:id", (req: Request, res: Response) => {
-    let id: number = parseInt(req.params.id ?? "0")
+router.get("/echo/:id", (req: Request, res: Response) => {
+    let value: string = req.params.id as string;
     try{
-        res.json(poems[id])
+        res.json({id: value});
     } catch (error: any) {
         console.error(`Error parsing JSON: ${error}`)
     }
    
 })
 
+router.post("/sum", (req: Request, res: Response) => {
+    const numbersArr: number[] = req.body.numbers
+    console.log(numbersArr);
+    let result: number = 0;
+    //for (let i = 0; i < numbersArr.length; i++) {
+      //  result += numbersArr[i]
+    //}
+    for (const num of numbersArr) {
+        result += num; 
+    }
+    res.json({sum: result})
+})
 
 export default router
